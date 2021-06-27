c57871abae5e2564e7a87ee1f5735c004921271e
ByungJun <pjoon357@naver.com>
2021-06-27 21:56:18 +0900

    JSX&PROPS js-map

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
+import PropTypes from "prop-types";
+
+function ToDo({name}){
+  return <h1>I will study {name}</h1>
+}
+
+ToDo.propTypes={
+  name: PropTypes.string.isRequired
+};
+
+const whats=[
+  {
+    id: 1,
+    name: "ReactJs"
+  },
+  {
+    id: 2,
+    name: "React Native"
+  }, 
+  {
+    id: 3,
+    name: "Js"
+  }];
 
 function App() {
   return (
     <div className="App">
-      
+      {whats.map(what=>(
+      <ToDo key={what.id} name={what.name}/>
+      ))}
     </div>
   );
 }
